62e4f43727da65db7ce965a925c1de25c0e69380
sbmn97 <sbmn97@yahoo.com>
2025-10-01 09:32:06 +0800

    First Commit of my product page

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('shows the product name', () => {
+  render(<App />);
+  expect(screen.getByText(/Super Sneakers/i)).toBeInTheDocument();
+});
+test('shows the Add to Cart button', () => {
+  render(<App />);
+  expect(screen.getByRole('button', { name: /add to cart/i })).toBeInTheDocument();
+});
